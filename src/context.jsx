b99587e7094277ff73d7e9d9063bcc8eb5b09e8d
lightnonstop <umichaeledet003@gmail.com>
2023-04-03 22:05:41 +0000

    switched to axios for data fetching

--- src/context.jsx
+++ src/context.jsx
@@ -1,8 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 const AppContext = React.createContext();
+import axios from "axios";
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 const AppProvider = ({ children }) => {
+
+
+    const fetchMeals = async (url) => {
+        try {
+            const {data} = await axios(url);
+            console.log(data);
+        } catch (error){
+            console.log(error.response);
+        }
+    }
+
+    useEffect(() => {
+        fetchMeals(allMealsUrl);
+    }, [])
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
+
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
 
 export { AppContext, AppProvider };
