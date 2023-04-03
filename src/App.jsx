c5014c102ee8659ac29a7216bb88edd87ac701c2
lightnonstop <umichaeledet003@gmail.com>
2023-04-03 22:51:37 +0000

    now using vanilla css

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Search from './components/Search.jsx';
+import Meals from './components/Meals.jsx';
+import Modal from './components/Modal.jsx';
+import Favorites from './components/Favorites.jsx';
+const App = () => {
+  return (
+    <main>
+      {/* <Search /> */}
+      {/* <Favorites /> */}
+      <Meals />
+      {/* <Modal /> */}
+    </main>
+  )
+}
+
+export default App
