bc7778728b2f6adb4704ff2556bf78bc8178c6ed
lightnonstop <umichaeledet003@gmail.com>
2023-04-05 15:47:36 +0000

    Fixed 'destroy is not a function bug' by removing return in useEffect hook

--- src/context.jsx
+++ src/context.jsx
@@ -1,25 +1,88 @@
 import React, { useContext, useEffect, useState } from "react";
 const AppContext = React.createContext();
 import axios from "axios";
-const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
-const AppProvider = ({ children }) => {
+const getFavouritesFromLocalStorage = () => {
+    let favourites = localStorage.getItem('favourites');
 
+    if(favourites){
+        favourites = JSON.parse(localStorage.getItem('favourites'));
+    } else{
+        favourites = [];
+    }
+    return favourites
+}
+
+const AppProvider = ({ children }) => {
+    const [loading, setLoading] = useState(false);
+    const [meals, setMeals] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const [showModal, setShowModal] = useState(false);
+    const [selectedMeal, setSelectedMeal] = useState(null);
+    const [favourites, setFavourites] = useState(getFavouritesFromLocalStorage());
 
     const fetchMeals = async (url) => {
+        setLoading(true);
         try {
             const {data} = await axios(url);
-            console.log(data);
+            data.meals ? setMeals(data.meals) : setMeals([]);
+            
         } catch (error){
             console.log(error.response);
         }
+        setLoading(false);
     }
 
+    const fetchRandomMeal = () => {
+        fetchMeals(randomMealUrl);
+    }
+
+    const selectMeal = (idMeal, favouriteMeal) => {
+        let meal;
+        if(favouriteMeal){
+            meal = favourites.find((meal) => meal.idMeal === idMeal)
+        } else {
+            meal = meals.find((meal) => meal.idMeal === idMeal)
+        }
+        setSelectedMeal(meal);
+        setShowModal(true);
+    }
+
+    const closeModal = () => {
+        setShowModal(false);
+    }
+
+    const addToFavourites = (idMeal) => {
+        
+        const alreadyFavourite = favourites.find((meal) => meal.idMeal === idMeal)
+        if (alreadyFavourite) return;
+        const meal = meals.find((meal) => meal.idMeal === idMeal)
+        const updatedFavourites = [...favourites, meal];
+        setFavourites(updatedFavourites);
+        localStorage.setItem('favourites', JSON.stringify(updatedFavourites));
+    }
+
+    const removeFromFavourites = (idMeal) => {
+        const updatedFavourites = favourites.filter((meal) => meal.idMeal !== idMeal);
+        setFavourites(updatedFavourites);
+        localStorage.setItem('favourites', JSON.stringify(updatedFavourites));
+    }
     useEffect(() => {
         fetchMeals(allMealsUrl);
     }, [])
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+
+
+    useEffect(() => {
+        if (searchTerm){
+            fetchMeals(`${allMealsUrl}${searchTerm}`)
+        };
+    }, [searchTerm])
+
+
+  return <AppContext.Provider value={{loading, meals, setSearchTerm, fetchRandomMeal, showModal, selectedMeal, selectMeal, closeModal, addToFavourites, removeFromFavourites, favourites}}>{children}</AppContext.Provider>;
 };
 
 
